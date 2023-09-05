f29daf5c2edb7b271cc5323cefda3636cfa21b53
khemchand-twt11 <gauravsharma6290852476@gmail.com>
2023-09-05 18:04:11 +0530

    welcome screen

--- FitWorkoutApp/src/screens/Welcome.js
+++ FitWorkoutApp/src/screens/Welcome.js
@@ -1,22 +1,26 @@
-import { Button, Text, View } from 'react-native'
-const data = [
-  { name: 'gaurav' },
-  { name: 'saurabh' },
-  { name: 'vivek' },
-  { name: 'shri' },
-  { name: 'manish' },
-]
-function Welcome({ navigation }) {
+import React from 'react'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+
+function Welcome() {
   return (
-    <View className='bottom-0 border-2 mt-0  w-full'>
-      <Text className='text-2xl'>hi hello</Text>
-      <Button
-        title='Get Started'
-        onPress={() => navigation.navigate('UserProfile')}
+    <View className='flex-1 justify-center items-center'>
+      <Image
+        source={require('../assets/fitman.jpg')}
+        className='h-screen w-full'
       />
-      {data.map((item, index) => (
-        <Text key={index}>{item.name}</Text>
-      ))}
+      <View className='absolute bottom-32 flex justify-center items-center w-full'>
+        <Text className='text-white text-4xl font-bold  font-mono mb-1'>
+          FitWorkout
+        </Text>
+        <Text className='mb-10 text-white font-medium text-center text-2xl w-full'>
+          Train Insane or Remain the Same.
+        </Text>
+        <TouchableOpacity className='bg-[#d5ff5f] max-w-md px-4 py-2 rounded-lg w-56'>
+          <Text className='text-black text-center font-bold text-lg'>
+            Get Started
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
